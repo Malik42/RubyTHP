590044b7c881b658c868abbad4a0cd36943e9987
Malik <malik@PC-MALIK.lan>
2019-01-04 20:35:31 +0100

    Exo 06 fini

--- exo_06.rb
+++ exo_06.rb
@@ -11,4 +11,4 @@ puts "Et en minutes ça fait : #{number_of_minutes_in_an_hour * number_of_hours_
 # Que se passe-t-il ? Peux-tu expliquer ?
 
 # Au debut du programme on initialise 3 varible, après on fait un calcule avec ces 3 variables et on affiche le resultat
-# Après avec l'ajout de la dernière ligne on affiche le resultat d'un calcul mais on obtient une erreur car on veut faire un calcul avec des variables qui n'existe pas qui ne sont pas initialise
+# Après avec l'ajout de la dernière ligne on affiche le resultat d'un calcul mais on obtient une erreur car on veut faire un calcul avec une variables qui n'existe pas qui ne sont pas initialise
