5bc8947ba3bb4d40b19c615d6fef89074a67c9eb
Malik <malik@PC-MALIK.lan>
2019-01-04 17:09:30 +0100

    Exo 7c fini

---
+++ exo_07_c.rb
@@ -0,0 +1,8 @@
+# Exercice demander a l'utilisateur
+
+user_name = gets.chomp
+puts user_name
+
+# Quelles sont les différences entre les trois programmes ?
+# Les difference entre les programmes sont l'utilisation, par exemple le a et le b sont plus userfriendly que le c qui attend juste une données mais ne dit pas a l'utilisateur quel données il doit saisir
+# Le a et le b sont presque identique juste le print u b change l'ésthetique du programme
