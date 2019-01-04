543301c4b2896b32c802900382522e54e75aa60e
Malik <malik@PC-MALIK.lan>
2019-01-04 16:35:50 +0100

    Exo 6 fini

---
+++ exo_06.rb
@@ -0,0 +1,14 @@
+# Exercice variables
+
+number_of_hours_worked_per_day = 10
+number_of_days_worked_per_week = 5
+number_of_weeks_in_THP = 11
+
+puts "Travail : #{number_of_hours_worked_per_day * number_of_days_worked_per_week * number_of_weeks_in_THP}"
+
+puts "Et en minutes ça fait : #{number_of_minutes_in_an_hour * number_of_hours_worked_per_day * number_of_days_worked_per_week * number_of_weeks_in_THP}"
+
+# Que se passe-t-il ? Peux-tu expliquer ?
+
+# Au debut du programme on initialise 3 varible, après on fait un calcule avec ces 3 variables et on affiche le resultat
+# Après avec l'ajout de la dernière ligne on affiche le resultat d'un calcul mais on obtient une erreur car on veut faire un calcul avec des variables qui n'existe pas qui ne sont pas initialise
