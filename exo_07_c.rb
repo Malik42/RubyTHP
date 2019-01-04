b5ef70c4f1cd7b61afca783d2d217dd6cd8c95e2
Malik <malik@PC-MALIK.lan>
2019-01-04 20:42:18 +0100

    Exo 7c fini

--- exo_07_c.rb
+++ exo_07_c.rb
@@ -5,4 +5,4 @@ puts user_name
 
 # Quelles sont les différences entre les trois programmes ?
 # Les difference entre les programmes sont l'utilisation, par exemple le a et le b sont plus userfriendly que le c qui attend juste une données mais ne dit pas a l'utilisateur quel données il doit saisir
-# Le a et le b sont presque identique juste le print u b change l'ésthetique du programme
+# Le a et le b sont presque identique juste le print du b change l'ésthetique du programme
