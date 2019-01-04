d89dc09363349c8fc7e1641bf35dda92bc0a1d00
Malik <malik@PC-MALIK.lan>
2019-01-04 20:33:02 +0100

    Exo 4 fini

--- exo_04.rb
+++ exo_04.rb
@@ -3,4 +3,4 @@
 # puts "Salut, ça farte
 
 # Que se passe-t-il ? Pourquoi ?
-# Le puts ne s'affiche pas on obtient un message d'erreur, il manque une doucle guillemet pour refermé le puts
+# Le puts ne s'affiche pas on obtient un message d'erreur, il manque une boucle guillemet pour refermé le puts
