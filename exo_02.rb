cfb0d370b954c4fe132937ba9355222d3632ba2f
Malik <malik@PC-MALIK.lan>
2019-01-04 15:59:58 +0100

    exo 2 fini

---
+++ exo_02.rb
@@ -0,0 +1,7 @@
+# Exercice bonjour monde 2
+
+puts "Bonjour, monde !"
+puts "Et avec une voix sexy, ça donne : Bonjour, monde !"
+
+# Peux-tu connaître la différence entre print et puts ?
+# Le puts fait un saut de ligne contrairemet a print
