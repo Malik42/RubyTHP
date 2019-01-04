6f6dc87726eb486ceffdbbd86a7d964f6887b9e9
Malik <malik@PC-MALIK.lan>
2019-01-04 16:02:22 +0100

    Exo 3 fini

---
+++ exo_03.rb
@@ -0,0 +1,7 @@
+# Exercice il ne dit pas bonjour
+
+puts "Bonjour, monde !"
+# puts "Et avec une voix sexy, ça donne : Bonjour, monde !"
+
+# Peux-tu me dire ce qu'il se passe ?
+# le caractères # défini un commentaire donc ma ligne 2 n'est pas interprété par l'ordinateur
