c33c01fce2683d9144514d7c25847ebd8dee4f03
Malik <malik@PC-MALIK.lan>
2019-01-04 16:06:57 +0100

    Exo 4 fini

---
+++ exo_04.rb
@@ -0,0 +1,6 @@
+# Exercice ça marche po
+
+# puts "Salut, ça farte
+
+# Que se passe-t-il ? Pourquoi ?
+# Le puts ne s'affiche pas on obtient un message d'erreur, il manque une doucle guillemet pour refermé le puts
