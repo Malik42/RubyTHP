841989e80e50133ce2b4bb368a0c04f882594629
Malik <malik@PC-MALIK.lan>
2019-01-04 17:17:49 +0100

    Exo 9 fini

---
+++ exo_09.rb
@@ -0,0 +1,7 @@
+# Exercice Un programme qui dit bonjour de manière complète
+
+puts "Quel est ton prénom"
+name = gets.chomp
+puts "Quel est ton nom"
+lastname = gets.chomp
+puts "Bonjour, #{name} #{lastname}"
