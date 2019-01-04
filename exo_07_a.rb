faa43b530e3405380abc64f40d2887190d3c8359
Malik <malik@PC-MALIK.lan>
2019-01-04 17:08:13 +0100

    Exo 7a fini

---
+++ exo_07_a.rb
@@ -0,0 +1,8 @@
+# Exercice demander a l'utilisateur
+
+puts "Bonjour, c'est quoi ton blase ?"
+user_name = gets.chomp
+puts user_name
+
+# Peux-tu me dire ce que fais gets.chomp ?
+# gets.chomp demande a l'utilisateur de saisir une donnée, quand on execute le code le programme attend que l'utilisateur saisit une la données
